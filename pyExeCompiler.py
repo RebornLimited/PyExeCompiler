063c76fdb3e0c2b95c25e88ce0cc6b46972c41c3
RebornLimited <95706178+RebornLimited@users.noreply.github.com>
2022-03-13 20:46:04 +0300

    Delete pyExeCompiler.py

--- pyExeCompiler.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def compile(file, icon):
-    os.system("pip install pyinstaller")
-
-    if icon == None:
-        os.system(f"pyinstaller -F {file}")
-    else:
-        os.system(f"pyinstaller -F -i \"{icon}\" {file}")
